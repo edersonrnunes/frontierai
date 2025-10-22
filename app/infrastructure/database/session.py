ee8fdd80bcde6d047b7a96af25d35233bcd45e3b
Ederson Nunes <edersonnunes@g.globo>
2025-10-22 15:55:45 -0300

    Alterado para conexão no DB PostgreSQL Supabase

--- app/infrastructure/database/session.py
+++ app/infrastructure/database/session.py
@@ -6,13 +6,40 @@ from dotenv import load_dotenv
 
 load_dotenv(verbose=True)
 
-## Conexão local para debug
-#SQLALCHEMY_DATABASE_URL = "postgresql://user:password@localhost:5432/mydatabase"
-## Conexão para utilizar dentro do Docker
-#SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@localhost:5432/mydatabase")
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL") 
+###########################################################################################################
+## Código para utilizar Supabase como banco de dados PostgreSQL
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# Fetch variables
+USER = os.getenv("user")
+PASSWORD = os.getenv("password")
+HOST = os.getenv("host")
+PORT = os.getenv("port")
+DBNAME = os.getenv("dbname")
+
+# Construct the SQLAlchemy connection string
+DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
+
+# Create the SQLAlchemy engine
+engine = create_engine(DATABASE_URL)
+
+# Test the connection
+try:
+    with engine.connect() as connection:
+        print("Connection successful!")
+except Exception as e:
+    print(f"Failed to connect: {e}")
+###########################################################################################################
+
+################################## Descomentar se não utilizar Supabase ###################################
+## Este código utiliza DB PostgreSQL utilizando containers Docker
+## Conexão local para debug (Container local)
+#SQLALCHEMY_DATABASE_URL = "postgresql://user:password@localhost:5432/mydatabase"
+## Conexão para utilizar dentro do Docker Compose
+#SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@localhost:5432/mydatabase")
+#SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL") 
+
+#engine = create_engine(SQLALCHEMY_DATABASE_URL)
+###########################################################################################################
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
