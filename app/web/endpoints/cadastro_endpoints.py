f4efdbf8e356a5c1d92a03f24d06c78524cf718f
Ederson Nunes <edersonnunes@g.globo>
2025-10-23 12:10:28 -0300

    Issues: CodeScanningAlert #1385 #3

--- app/web/endpoints/cadastro_endpoints.py
+++ app/web/endpoints/cadastro_endpoints.py
@@ -48,8 +48,8 @@ def read_root():
 def health_check(
     repo: PessoaRepositories.PessoaRepository = Depends(get_pessoa_repository)
 ):
-    pr = PessoaUseCases.GetPessoaById(repo)
-    pessoa = pr.execute(pessoa_id=1)
+    pessoa_use_cases = PessoaUseCases.GetPessoaById(repo)
+    pessoa = pessoa_use_cases.execute(pessoa_id=1)
     if not pessoa:
         return {"status": "Database connection failed"}, status.HTTP_500_INTERNAL_SERVER_ERROR
     return {"status": "ok"}
@@ -62,8 +62,8 @@ def create_item(
     repo: ItemRepositories.ItemRepository = Depends(get_item_repository),
 ):
     print(f"User '{current_user.username}' creating item with data: {item_dto}")
-    itemUseCases = ItemUseCases.CreateItemUseCase(repo)
-    result = itemUseCases.execute(item_dto)
+    item_use_cases = ItemUseCases.CreateItemUseCase(repo)
+    result = item_use_cases.execute(item_dto)
     return result
 
 @router.get("/items/", response_model=list[ItemDtos.Item], tags=["Items"])
@@ -73,8 +73,8 @@ def read_items(
     repo: ItemRepositories = Depends(get_item_repository)
 ):
     print(f"User '{current_user.username}' reading items with skip={skip} and limit={limit}")
-    itemUseCases = ItemUseCases.ListItemsUseCase(repo)
-    items = itemUseCases.execute(skip=skip, limit=limit)
+    item_use_cases = ItemUseCases.ListItemsUseCase(repo)
+    items = item_use_cases.execute(skip=skip, limit=limit)
     return items
 
 ## Pessoa
@@ -86,8 +86,8 @@ def create_pessoa_for_usuario(
     pessoa_repo: PessoaRepositories.PessoaRepository = Depends(get_pessoa_repository)
 ):
     print(f"Creating pessoa for usuario ID: {usuario_id} with data: {pessoa_dto}")
-    pessoaUseCases = PessoaUseCases.CreatePessoaUsuario(pessoa_repo, usuario_repo)
-    return pessoaUseCases.execute(usuario_id=usuario_id, pessoa_create=pessoa_dto)
+    pessoa_use_cases = PessoaUseCases.CreatePessoaUsuario(pessoa_repo, usuario_repo)
+    return pessoa_use_cases.execute(usuario_id=usuario_id, pessoa_create=pessoa_dto)
 
 
 @router.post("/pessoas/", response_model=PessoaDtos.PessoaBase, status_code=status.HTTP_201_CREATED, tags=["Pessoas"])
@@ -101,8 +101,8 @@ def create_pessoa(
     Requer um token JWT válido no header `Authorization: Bearer <token>`.
     """
     print(f"User '{current_user.username}' creating pessoa with data: {pessoa_dto}")
-    pessoaUseCases = PessoaUseCases.CreatePessoa(repo)
-    result = pessoaUseCases.execute(pessoa_dto)
+    pessoa_use_cases = PessoaUseCases.CreatePessoa(repo)
+    result = pessoa_use_cases.execute(pessoa_dto)
     return result
 
 @router.get("/pessoas/", response_model=list[PessoaDtos.Pessoa], tags=["Pessoas"])
@@ -116,8 +116,8 @@ def list_pessoas(
     Requer um token JWT válido no header `Authorization: Bearer <token>`.
     """
     print(f"User '{current_user.username}' reading pessoas with skip={skip} and limit={limit}")
-    pessoaUseCases = PessoaUseCases.ListPessoa(repo)
-    pessoas = pessoaUseCases.execute(skip=skip, limit=limit)
+    pessoa_use_cases = PessoaUseCases.ListPessoa(repo)
+    pessoas = pessoa_use_cases.execute(skip=skip, limit=limit)
     return pessoas
 
 @router.get("/pessoas/{pessoa_id}", response_model=PessoaDtos.PessoaBase, tags=["Pessoas"])
@@ -127,8 +127,8 @@ def get_pessoa_by_id(
     repo: PessoaRepositories.PessoaRepository = Depends(get_pessoa_repository)
 ):
     print(f"User '{current_user.username}' get pessoa with pesso_id")
-    pessoaUseCases = PessoaUseCases.GetPessoaById(repo)
-    pessoa = pessoaUseCases.execute(pessoa_id=pessoa_id)
+    pessoa_use_cases = PessoaUseCases.GetPessoaById(repo)
+    pessoa = pessoa_use_cases.execute(pessoa_id=pessoa_id)
     return pessoa
 
 
@@ -139,8 +139,8 @@ def update_pessoa(
     repo: PessoaRepositories.PessoaRepository = Depends(get_pessoa_repository)
 ):
     print(f"Updating endereco with ID: {pessoa_id} with data: {pessoa_dto}")
-    pessoaUseCases = PessoaUseCases.UpdatePessoa(repo)
-    return pessoaUseCases.execute(pessoa_id=pessoa_id, pessoa_update=pessoa_dto)
+    pessoa_use_cases = PessoaUseCases.UpdatePessoa(repo)
+    return pessoa_use_cases.execute(pessoa_id=pessoa_id, pessoa_update=pessoa_dto)
 
 @router.delete("/pessoas/{pessoa_id}", status_code=status.HTTP_204_NO_CONTENT, tags=["Pessoas"])
 def delete_pessoa(
@@ -148,8 +148,8 @@ def delete_pessoa(
     repo: PessoaRepositories.PessoaRepository = Depends(get_pessoa_repository)
 ):
     logger.info(f"Deleting pessoa with ID: {pessoa_id}")
-    pessoaUseCases = PessoaUseCases.DeletePessoa(repo)
-    pessoaUseCases.execute(pessoa_id=pessoa_id)
+    pessoa_use_cases = PessoaUseCases.DeletePessoa(repo)
+    pessoa_use_cases.execute(pessoa_id=pessoa_id)
     return
 
 ## Endereço
@@ -161,8 +161,8 @@ def create_endereco_for_pessoa(
     pessoa_repo: PessoaRepositories.PessoaRepository = Depends(get_pessoa_repository)
 ):
     print(f"Creating endereco for pessoa ID: {pessoa_id} with data: {endereco_dto}")
-    enderecoUseCases = EnderecoUseCases.CreateEndereco(endereco_repo, pessoa_repo)
-    return enderecoUseCases.execute(pessoa_id=pessoa_id, endereco_create=endereco_dto)
+    endereco_use_cases = EnderecoUseCases.CreateEndereco(endereco_repo, pessoa_repo)
+    return endereco_use_cases.execute(pessoa_id=pessoa_id, endereco_create=endereco_dto)
 
 @router.get("/pessoas/{pessoa_id}/enderecos/", response_model=list[EnderecoDtos.Endereco], tags=["Endereços"])
 def read_enderecos_for_pessoa(
@@ -171,8 +171,8 @@ def read_enderecos_for_pessoa(
     repo: EnderecoRepositories.EnderecoRepository = Depends(get_endereco_repository)
 ):
     print(f"Reading enderecos for pessoa ID: {pessoa_id} with skip={skip} and limit={limit}")
-    enderecoUseCases = EnderecoUseCases.ListEnderecos(repo)
-    return enderecoUseCases.execute(pessoa_id=pessoa_id, skip=skip, limit=limit)
+    endereco_use_cases = EnderecoUseCases.ListEnderecos(repo)
+    return endereco_use_cases.execute(pessoa_id=pessoa_id, skip=skip, limit=limit)
 
 @router.put("/enderecos/{endereco_id}", response_model=EnderecoDtos.Endereco, tags=["Endereços"])
 def update_endereco(
@@ -181,8 +181,8 @@ def update_endereco(
     repo: EnderecoRepositories.EnderecoRepository = Depends(get_endereco_repository)
 ):
     print(f"Updating endereco with ID: {endereco_id} with data: {endereco_dto}")
-    enderecoUseCases = EnderecoUseCases.UpdateEndereco(repo)
-    return enderecoUseCases.execute(endereco_id=endereco_id, endereco_update=endereco_dto)
+    endereco_use_cases = EnderecoUseCases.UpdateEndereco(repo)
+    return endereco_use_cases.execute(endereco_id=endereco_id, endereco_update=endereco_dto)
 
 @router.delete("/enderecos/{endereco_id}", status_code=status.HTTP_204_NO_CONTENT, tags=["Endereços"])
 def delete_endereco(
@@ -190,6 +190,6 @@ def delete_endereco(
     repo: EnderecoRepositories.EnderecoRepository = Depends(get_endereco_repository)
 ):
     print(f"Deleting endereco with ID: {endereco_id}")
-    enderecoUseCases = EnderecoUseCases.DeleteEndereco(repo)
-    enderecoUseCases.execute(endereco_id=endereco_id)
+    endereco_use_cases = EnderecoUseCases.DeleteEndereco(repo)
+    endereco_use_cases.execute(endereco_id=endereco_id)
     return
